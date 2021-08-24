5a8d0127a83dec40c5535a8e9b87e545bda325f2
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-08-23 22:32:48 -0700

    added all features and ready to deploy

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,9 +1,33 @@
 <template>
-  <p>Chatroom</p>
+  <div class="container">
+    <Navbar />
+    <ChatWindow />
+    <NewChatForm />
+  </div>
 </template>
 
 <script>
-export default {};
+import Navbar from '../components/Navbar.vue';
+import getUser from '../composables/getUser';
+import NewChatForm from '../components/NewChatForm.vue';
+import ChatWindow from '../components/ChatWindow.vue';
+
+import { useRouter } from 'vue-router';
+import { watch } from '@vue/runtime-core';
+
+export default {
+  components: { Navbar, NewChatForm, ChatWindow },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' });
+      }
+    });
+  },
+};
 </script>
 
 <style></style>
